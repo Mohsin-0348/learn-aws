c46b06eac4eee5bd0509a5646c99042a8fed2e35
jim <mohsin.mahbub.jim@gmail.com>
2021-09-01 15:31:54 +0600

    Good to go

--- content/views.py
+++ content/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
-@login_required
+# @login_required
 def hello(request):
     return render(request, 'hello.html')
