abdff459f376d1cd033945a5b7340fbfcf08f513
jim <mohsin.mahbub.jim@gmail.com>
2021-09-01 15:34:52 +0600

    Good to go

--- learn_pro/settings.py
+++ learn_pro/settings.py
@@ -75,8 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'learn_pro.wsgi.application'
 
-AUTH_USER_MODEL = 'users.CustomUser'
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
