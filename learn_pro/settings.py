39d0221e714c9155fbf37403eb29a5f841b67c0e
jim <mohsin.mahbub.jim@gmail.com>
2021-09-01 11:05:52 +0600

    Changes in static

--- learn_pro/settings.py
+++ learn_pro/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'learn_pro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
