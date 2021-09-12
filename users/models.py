6413f515822e03383272fec53010667715faec6b
jim <mohsin.mahbub.jim@gmail.com>
2021-09-12 18:09:37 +0600

    some deleted

--- users/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class CustomUser(AbstractUser):
-    photo = models.FileField(blank=True, null=True, upload_to='profile_photo')
