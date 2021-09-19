c8dc594bcbb1dcd313a914e807e778a839bdfbe2
jim <mohsin.mahbub.jim@gmail.com>
2021-09-19 10:17:35 +0600

    changes on settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -89,22 +89,22 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'test',
-#         'USER': 'postgres',
-#         'PASSWORD': 'jim12345',
-#         'HOST': 'localhost',
-#         'PORT': ''
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'jim12345',
+        'HOST': 'localhost',
+        'PORT': ''
+    }
+}
 
 
 # Password validation
