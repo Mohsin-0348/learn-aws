c13ea090ce8cecf0cf7437b58e5c253a65176160
jim <mohsin.mahbub.jim@gmail.com>
2021-09-27 12:07:23 +0600

    cors header added

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+
+import corsheaders.middleware
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,8 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'graphene_django',
     'corsheaders',
+    'graphene_django',
 
     'users',
     'chat',
@@ -99,12 +101,14 @@ AUTH_USER_MODEL = 'users.User'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -168,6 +172,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DB_PREFIX = 'w3chat'
 
+# Cores origin
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
+CORS_ORIGIN_ALLOW_ALL = True
+
 # graphene config
 GRAPHENE = {
     'SCHEMA': 'mysite.schema.schema',
@@ -175,12 +185,6 @@ GRAPHENE = {
         'mysite.middlewares.W3AuthMiddleware'
     ]
 }
-
-# Cores origin
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
-]
-CORS_ORIGIN_ALLOW_ALL = True
 
 # mail config
 SENDGRID_API_KEY = config('SENDGRID_API_KEY', None)
