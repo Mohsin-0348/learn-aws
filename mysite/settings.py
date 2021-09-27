2ba0a60f353f84ccb2e4f70dcb57d9d1319a8023
jim <mohsin.mahbub.jim@gmail.com>
2021-09-27 12:09:18 +0600

    cors header added

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-import corsheaders.middleware
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
